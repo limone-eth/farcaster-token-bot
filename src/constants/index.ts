76d7a322c002c6f347298ee921a2f36c3e0345ba
lim0n3 <simonestaffa96@gmail.com>
2023-12-19 10:27:58 +0100

    change env variables

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,8 +7,12 @@ export const constants = {
     "0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67",
   WRAPPED_ETH_SMART_CONTRACT_ADDRESS:
     "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-  TOKEN_ADDRESS: process.env.TOKEN_ADDRESS,
-  TOKEN_DECIMALS: parseInt(process.env.TOKEN_DECIMALS),
-  TOKEN_SYMBOL: process.env.TOKEN_SYMBOL,
-  TOKEN_UNISWAP_POOL_ADDRESS: process.env.TOKEN_UNISWAP_POOL_ADDRESS,
+  // the address of the token you want to track
+  TOKEN_ADDRESS: "0xd7c1eb0fe4a30d3b2a846c04aa6300888f087a5f",
+  // the decimals of the token you want to track
+  TOKEN_DECIMALS: 18,
+  // the symbol of the token you want to track
+  TOKEN_SYMBOL: "POINTS",
+  // the address of the Uniswap V3 Pool you want to track (for swaps)
+  TOKEN_UNISWAP_POOL_ADDRESS: "0xa424817985051ccda51eff2dc7998b5d68079215",
 };
